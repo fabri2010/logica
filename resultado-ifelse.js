27c37c2f539b22d4f1148612040a20e190d1e06a
fabri2010 <fabricio.vaz.santos@escola.pr.gov.br>
2025-07-01 08:04:36 -0300

    Create resultado-ifelse.js

---
+++ resultado-ifelse.js
@@ -0,0 +1,7 @@
+let avaliacao = 7; // Ex. de nota pode ter casa decimal assim 0.00 - não use vírgula!
+
+if (avaliacao > 8) { //  Incluir a nota de aprovação
+  console.log("Você está aprovado. Parabéns!");
+} else {
+  console.log("Infelizmente nao esta aprovado."); // Incluir msg da última condição
+};
